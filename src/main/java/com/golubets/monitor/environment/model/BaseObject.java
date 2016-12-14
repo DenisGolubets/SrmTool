d5a03ff0da66ef9cfb86f9624244c19f5ecb620b
DenisGolubets <denis.golubets@gmail.com>
2016-12-14 17:07:15 +0700

    Restructuring

--- src/main/java/com/golubets/monitor/environment/model/BaseObject.java
+++
@@ -1,9 +0,0 @@
-package com.golubets.monitor.environment.model;
-
-import java.io.Serializable;
-
-/**
- * Created by golubets on 13.09.2016.
- */
-public interface BaseObject extends Serializable {
-}
